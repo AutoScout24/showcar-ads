02a8a3a6f4bcfdffbf9225518972e04a9b1c7339
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-11-02 10:04:08 +0000

    Release

--- src/js/indexexchange.js
+++ src/js/indexexchange.js
@@ -6,7 +6,10 @@ export const loadIndexExchange = () => {
     const isAT = tld === 'at';
     const isIT = tld === 'it';
     const isNL = tld === 'nl';
-    const isIndexExchangeEnabled = isDE || isAT || isIT || isNL || location.search.indexOf('indexexchange=1') >= 0 || document.cookie.indexOf('indexexchange=1') >= 0;
+    const isBE = tld === 'be';
+    const isFR = tld === 'fr';
+    const isIndexExchangeEnabled = isDE || isAT || isIT || isNL || isBE || isFR
+        || location.search.indexOf('indexexchange=1') >= 0 || document.cookie.indexOf('indexexchange=1') >= 0;
 
     if (!isIndexExchangeEnabled) return false;
 
@@ -14,4 +17,6 @@ export const loadIndexExchange = () => {
     else if (isAT) loadScript('//js-sec.indexww.com/ht/p/185725-71871796647060.js');
     else if (isIT) loadScript('//js-sec.indexww.com/ht/p/185725-124648354720881.js');
     else if (isNL) loadScript('//js-sec.indexww.com/ht/p/185725-60876680248506.js');
+    else if (isBE) loadScript('//js-sec.indexww.com/ht/p/185725-36693753739424.js');
+    else if (isFR) loadScript('//js-sec.indexww.com/ht/p/185725-93868358308799.js');
 };
