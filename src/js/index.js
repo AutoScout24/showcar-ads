af9e8feb5bc2ee92ea30a3638d119025257a79e2
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2017-11-24 14:32:08 +0000

    Release

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import { loadScript, ready as domready } from './dom';
 import { loadIndexExchange } from './indexexchange';
 import { loadAPS } from './aps';
 import * as OpenX from './openx';
+import * as NewOpenX from './new-openx';
 
 waitUntilAdsCanBeLoaded()
     .then(domready)
@@ -18,59 +19,63 @@ waitUntilAdsCanBeLoaded()
         loadAPS();
     })
     .then(() => {
-        const tld = location.hostname.split('.').pop();
-        const htmlLang = document.getElementsByTagName('html')[0].getAttribute('lang');
-        const lang = htmlLang.split('-')[0];
-        const useOpenX = OpenX.isEnabled(tld);
+        if (NewOpenX.isEnabled()) {
+            return NewOpenX.init().then(() => loadScript('https://www.googletagservices.com/tag/js/gpt.js'));
+        } else {
+            const tld = location.hostname.split('.').pop();
+            const htmlLang = document.getElementsByTagName('html')[0].getAttribute('lang');
+            const lang = htmlLang.split('-')[0];
+            const useOpenX = OpenX.isEnabled(tld);
 
 
-        if (!useOpenX) {
-            loadScript('https://www.googletagservices.com/tag/js/gpt.js');
-        }
-
-        if (useOpenX) {
-            const convertSizes = sizes => {
-                return JSON.parse(sizes).filter(x => Array.isArray(x) && x.length === 2).map(x => x.join('x'));
-            };
-
-            const activeSlots = Array.from(document.querySelectorAll('as24-ad-slot[sizes]:not([sizes="[]"]):not([out-of-page]):not([immediate]):not([openx-ignore])'));
-
-            window.OX_dfp_ads = activeSlots.map(element => [element.getAttribute('ad-unit'), convertSizes(element.getAttribute('sizes')), element.children[0].id]);
-
-            window.OX_cmds = window.OX_cmds || [];
-            window.OX_cmds.push(function () {
-                var oAR = (window.OX && window.OX.AdRequest);
-                if (oAR) {
-                    window.OX.AdRequest = function () {
-                        var ret = oAR.apply(this, arguments);
-                        const targeting = getTargetingData('as24-ad-targeting');
-
-                        if (targeting.splz) {
-                            this.addVariable('splz', targeting.splz);
-                        }
-
-                        if (targeting.zip2) {
-                            this.addVariable('zipcode', targeting.zip2);
-                        }
-
-                        return ret;
-                    };
-
-                    window.OX.prototype = oAR.prototype;
-                }
-            });
-
-            const openxScriptUrl = OpenX.getScriptUrl(tld, lang);
-            loadScript(openxScriptUrl);
-
-            var oxTimeout;
-            const oxCallback = () => {
-                clearTimeout(oxTimeout);
+            if (!useOpenX) {
                 loadScript('https://www.googletagservices.com/tag/js/gpt.js');
-            };
+            }
 
-            oxTimeout = setTimeout(oxCallback, 1500);
-            window.OX_dfp_options = { callback: oxCallback };
+            if (useOpenX) {
+                const convertSizes = sizes => {
+                    return JSON.parse(sizes).filter(x => Array.isArray(x) && x.length === 2).map(x => x.join('x'));
+                };
+
+                const activeSlots = Array.from(document.querySelectorAll('as24-ad-slot[sizes]:not([sizes="[]"]):not([out-of-page]):not([immediate]):not([openx-ignore])'));
+
+                window.OX_dfp_ads = activeSlots.map(element => [element.getAttribute('ad-unit'), convertSizes(element.getAttribute('sizes')), element.children[0].id]);
+
+                window.OX_cmds = window.OX_cmds || [];
+                window.OX_cmds.push(function() {
+                    var oAR = (window.OX && window.OX.AdRequest);
+                    if (oAR) {
+                        window.OX.AdRequest = function() {
+                            var ret = oAR.apply(this, arguments);
+                            const targeting = getTargetingData('as24-ad-targeting');
+
+                            if (targeting.splz) {
+                                this.addVariable('splz', targeting.splz);
+                            }
+
+                            if (targeting.zip2) {
+                                this.addVariable('zipcode', targeting.zip2);
+                            }
+
+                            return ret;
+                        };
+
+                        window.OX.prototype = oAR.prototype;
+                    }
+                });
+
+                const openxScriptUrl = OpenX.getScriptUrl(tld, lang);
+                loadScript(openxScriptUrl);
+
+                var oxTimeout;
+                const oxCallback = () => {
+                    clearTimeout(oxTimeout);
+                    loadScript('https://www.googletagservices.com/tag/js/gpt.js');
+                };
+
+                oxTimeout = setTimeout(oxCallback, 1500);
+                window.OX_dfp_options = {callback: oxCallback};
+            }
         }
     })
     .catch(e => {
---
+++ src/js/new-openx.js
@@ -0,0 +1,84 @@
+import { registerHBLite } from './openx-hb-light-min';
+
+export const isEnabled = () => {
+    const isFeatureFlagOn = location.hash.indexOf('ads-use-new-openx') >= 0;
+    return isFeatureFlagOn;
+};
+
+const oxAdunitMap = {
+    '/4467/ms24_de/Detailpage/SkyRight': 538655030,
+    '/4467/ms24_de/Listpage/Contentbanner_2': 538615014,
+    '/4467/ms24_de/Listpage/Contentbanner_3': 538615009,
+    '/4467/ms24_de/Listpage/Contentbanner_4': 538615011,
+    '/4467/ms24_de/Listpage/Contentbanner_1': 538615007,
+    '/4467/ms24_de/Detailpage/GalleryMR': 538621138,
+    '/4467/ms24_de/Listpage/SkyRight': 538615016,
+    '/4467/as24_de/Listpage/Top': 538615010,
+    '/4467/as24_de/Listpage/SkyLeft': 538473049,
+    '/4467/as24_de/Detailpage/SkyRight': 538665959,
+    '/4467/ms24_de/Detailpage': 538473562,
+    '/4467/ms24_de/Listpage/Top': 538615015,
+    '/4467/ms24_de/Listpage/SkyLeft': 538473560,
+    '/4467/ms24_de/Listpage': 538473559,
+    '/4467/ms24_de/Homepage': 538624366,
+    '/4467/as24_de/Listpage': 538473052,
+    '/4467/as24_de/Detailpage/Top': 538621133,
+    '/4467/as24_de/Detailpage/GalleryMR': 538621136,
+    '/4467/as24_de/Gallery': 538473564,
+    '/4467/as24_de/Detailpage_HPA': 538473051,
+    '/4467/as24_de/Detailpage/Contentbanner_1': 538655031,
+    '/4467/ms24_de/Detailpage/SkyLeft': 538621139,
+    '/4467/as24_de/Listpage/SkyRight': 538615005,
+    '/4467/as24_de/Detailpage/Contentbanner_2': 538655032,
+    '/4467/as24_de/Detailpage': 538473050,
+    '/4467/as24_de/Listpage/Contentbanner_1': 538615008,
+    '/4467/as24_de/Listpage/Contentbanner_2': 538615012,
+    '/4467/as24_de/Listpage/Contentbanner_3': 538615006,
+    '/4467/ms24_de/Detailpage/Contentbanner_1': 538621135,
+    '/4467/as24_de/Listpage/Contentbanner_4': 538615013,
+    '/4467/ms24_de/Detailpage/Contentbanner_2': 538621137,
+    '/4467/ms24_de/Detailpage/Top': 538621140,
+    '/4467/as24_de/Detailpage/SkyLeft': 538621134
+};
+
+//setting up the config for hb-lite
+const bidderConfig = {
+    timeLimit: 1500,
+    deliveryDomain: 'scout24-d.openx.net',
+    singleRequest: true,
+    contentTopidId: '99',
+    adMappings: [],
+};
+
+export const init = () => {
+    registerHBLite();
+
+    const activeSlots = Array.from(document.querySelectorAll('as24-ad-slot[sizes]:not([sizes="[]"]):not([out-of-page]):not([immediate]):not([openx-ignore])'));
+
+    const targeting = window.getTargetingData('as24-ad-targeting');
+    const splz = targeting.splz ? {'splz': targeting.splz} : {};
+    const zip2 = targeting.zip2 ? {'zip2': targeting.zip2} : {};
+    const customKeyValuePairs = Object.assign(splz, zip2);
+
+    activeSlots.forEach(activeSlot => {
+        const dfpAdUnitKey = activeSlot.getAttribute('ad-unit');
+        const oxAdunitValue = oxAdunitMap[dfpAdUnitKey];
+
+        if (!oxAdunitValue) {
+            return false;
+        }
+
+        bidderConfig.adMappings.push({
+            type: 'display',
+            divId: activeSlot.children[0].id,
+            adUnit: dfpAdUnitKey,
+            sizes: JSON.parse(activeSlot.getAttribute('sizes')),
+            oxAdUnitId: oxAdunitValue,
+            customKeyValuePairs: customKeyValuePairs
+        });
+    });
+
+    window.oxhbjs = new window.HeaderBidder(bidderConfig);
+
+    return window.oxhbjs.getBids();
+};
