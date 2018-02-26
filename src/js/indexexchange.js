52326e74edf62b71da4cdb0c4d417f80d3c15fa3
Travis CI <#AS24-Web-Experience-Team-ds@scout24.com>
2018-02-26 16:33:07 +0000

    Release

--- src/js/indexexchange.js
+++ src/js/indexexchange.js
@@ -16,12 +16,12 @@ export const loadIndexExchange = () => {
     else if (isFR) loadScript('//js-sec.indexww.com/ht/p/185725-93868358308799.js');
     else if (isES) loadScript('//js-sec.indexww.com/ht/p/185725-80679730502062.js');
     else if (isBG && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-21306986011839.js');
-    else if (isCOM && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-16908939592252.js');
+    else if (isCOM) loadScript('//js-sec.indexww.com/ht/p/185725-16908939592252.js');
     else if (isTR && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-32302102478062.js');
     else if (isUA && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-65287451382876.js');
     else if (isCZ && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-78481591103080.js');
     else if (isHR && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-192830800486883.js');
-    else if (isLU && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-107068893625728.js');
+    else if (isLU) loadScript('//js-sec.indexww.com/ht/p/185725-107068893625728.js');
     else if (isPL && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-146651312312317.js');
     else if (isRO && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-177437637966217.js');
     else if (isRU && isFeatureFlagOn) loadScript('//js-sec.indexww.com/ht/p/185725-181835684532677.js');
