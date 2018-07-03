ff7e6ae9bbf6d2efefaa5adea67bd74b2f30ae77
Florian Goessler <florian.goessler@scout24.com>
2018-07-03 09:57:50 +0200

    ADS-1441: Add page id tag and checks to performance logging

--- src/js/perf-logging.js
+++ src/js/perf-logging.js
@@ -2,30 +2,41 @@
 const remotePerformanceLoggingEnabled = true;
 const remoteLoggingURL = 'https://osa-logging.s24-media.immobilienscout24.de/prod/logs/';
 
+const as24DetailPageRegex = /https:\/\/www.autoscout24.de\/angebote.*/;
+const as24ListPageRegex = /https:\/\/www.autoscout24.de\/ergebnisse.*/;
+
 const samplingRate = 0.1;
 const samplingGroup = Math.random();
 let sendPerformanceLogsTimer = null;
 const performanceLogData = {};
 
 export const recordPerfEvent = (perfEventName) => {
-    if (perfEventName && remotePerformanceLoggingEnabled && samplingGroup <= samplingRate && performanceLogData[perfEventName] === undefined) {
+    if (perfEventName && remotePerformanceLoggingEnabled && samplingGroup <= samplingRate && performanceLogData[perfEventName] === undefined && domainCheck()) {
         performanceLogData[perfEventName] = getTimeSincePageStart();
         queueSendingCollectedPerformanceLogsIfNeeded();
-        console.log('[AS24 ShowCar Ads] @' + performanceLogData[perfEventName] + 'ms: ' + perfEventName);
+        // console.log('[AS24 ShowCar Ads] @' + performanceLogData[perfEventName] + 'ms: ' + perfEventName);
     }
 };
 
 const queueSendingCollectedPerformanceLogsIfNeeded = () => {
     if (!sendPerformanceLogsTimer) {
         sendPerformanceLogsTimer = setTimeout(() => {
-            console.log('[AS24 ShowCar Ads] @ ' + getTimeSincePageStart() + 'ms: Sending performance logs...');
+            // console.log('[AS24 ShowCar Ads] @ ' + getTimeSincePageStart() + 'ms: Sending performance logs...');
+            let pageId = 'unknown';
+            pageId = as24DetailPageRegex.test(window.location.href) ? 'as24-de-detail' : pageId;
+            pageId = as24ListPageRegex.test(window.location.href) ? 'as24-de-list' : pageId;
             POST(remoteLoggingURL + 'showcar-performance', {
                 performanceLogData: performanceLogData,
+                pageId: pageId,
                 referrer: window.location.origin,
                 userAgent: navigator.userAgent
             });
         }, 10000);
     }
+};
+
+const domainCheck = () => {
+    return as24DetailPageRegex.test(window.location.href) || as24ListPageRegex.test(window.location.href)
 };
 
 const getTimeSincePageStart = () => {
